68551c0553d8383e1dea1e5e901b02fcdf76ec77
Chloe <cre3363@g.rit.edu>
2022-05-26 18:24:19 -0400

    Idle and walking animations

--- BeatEmUpRhythm/Assets/Scripts/Player.cs
+++ BeatEmUpRhythm/Assets/Scripts/Player.cs
@@ -6,20 +6,39 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 1.5f;
+    private float speed = 1f;
     private PlayerInput playerInput;
     private InputAction moveAction;
+    public Animator animator;
+
+    //private enum MovementState {idle, walk}
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
-        //Debug.Log(moveAction.ReadValue<Vector2>());
     }
 
     private void Update()
     {
-        //Debug.Log(moveAction.ReadValue<Vector2>());
+        //MovementState state;
+
+        // Movement
         transform.Translate(new Vector3(moveAction.ReadValue<Vector2>().x, moveAction.ReadValue<Vector2>().y, 0) * speed * Time.deltaTime);
+
+
+        // Animation stuff
+        animator.SetFloat("Horizontal", moveAction.ReadValue<Vector2>().x);
+        animator.SetFloat("Vertical", moveAction.ReadValue<Vector2>().y);
+        animator.SetFloat("Magnitude", moveAction.ReadValue<Vector2>().magnitude);
+
+        if (moveAction.ReadValue<Vector2>().x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveAction.ReadValue<Vector2>().x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
